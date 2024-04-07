ab0e8c8a57f1c15ab1d081df5dcd2c1e58735ca4
vikey725 <vikash.kumar725@gmail.com>
2024-04-08 00:18:16 +0530

    blog website initial commit

---
+++ myblog/blog/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Post
+
+def post_list(request):
+    post_list = Post.published.all()
+
+    paginator = Paginator(post_list, 1)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, "blog/post/list.html", {"posts": posts})
+
+def post_detail(request, year, month, day, post):
+    # try:
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No post found.")
+
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post,
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day)
+    
+    return render(request, "blog/post/detail.html", {"post": post})
